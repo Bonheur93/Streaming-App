c44fd897b7ad5eefde24f77c5e13f349fe799ddc
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-08-31 14:34:53 +0200

    SearchItems

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Login from "./Login";
 import Asider from "./Asider";
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
@@ -15,38 +15,46 @@ function Body() {
     const RESPONSE_TYPE = "token"
     const [token, setToken] = useState("")
 
+    const [searchKey, setSearchKey] = useState("")
+    const [artists, setArtists] = useState([])
+
     const searchArtists = async (e) => {
         e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+        const { data } = await axios.get("https://api.spotify.com/v1/search", {
             headers: {
                 Authorization: `Bearer ${token}`
             },
             params: {
                 q: searchKey,
-                type: "artist"
+                type: "track"
             }
         })
-    
-        setArtists(data.artists.items)
-    
 
-    const [searchKey, setSearchKey] = useState("")
-    const [artists, setArtists] = useState([])
+        setArtists(data.tracks.items)
+    }
+    const renderArtists = () => {
+        return artists.map(artist => (
+            <div key={artist.id}>
+                {artist.album.images.length ? <img width={"25%"} src={artist.album.images[0].url} alt=""/> : <div>No Image</div>}
+                {artist.uri}
+            </div>
+        ))
+    }
 
 
     useEffect(() => {
         const hash = window.location.hash
         let token = window.localStorage.getItem("token")
-    
+
         if (!token && hash) {
             token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-    
+
             window.location.hash = ""
             window.localStorage.setItem("token", token)
         }
-    
+
         setToken(token)
-    
+
     }, [])
 
     return (
@@ -59,13 +67,21 @@ function Body() {
 
             <div className="container" >
                 <header>
-                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Connectez-vous sur spotify</a>
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Connectez-vous sur spotify</a>
+                    <form onSubmit={searchArtists}>
+                        <input type="text" onChange={e => setSearchKey(e.target.value)} />
+                        <button type={"submit"}>Search</button>
+                    </form>
+                    <div className="artist">
+                    {renderArtists()}
+                    </div>
+                    
                 </header>
 
                 <h1 className="connect">Welcome to the Sky Music</h1>
-                    
+
                 <div className="login" >
-                   
+
                     <Login />
                 </div>
 

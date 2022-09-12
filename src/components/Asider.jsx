25093d6d3ae8668945444383ae212edc3e3a60f3
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-09-12 13:38:12 +0200

    Style Button Search

--- src/components/Asider.jsx
+++ src/components/Asider.jsx
@@ -16,27 +16,7 @@ function Asider() {
         <img className="bonlogo" src="bonlogo.jpg" alt="" />
         <p>Authentic Music</p>
       </div>
-      <div className="bibliothec">
-        <div className="containerLibrairie">
-        <div className="Library">
-          <img className="library" src="home.svg" alt="" />
-          <a href="#" className="lienRef"><h5>Accueil</h5></a>
-        </div>
-        <div className="Library">
-          <img className="library" src="library.svg" alt="" />
-          <a href="#" className="lienRef"><h5>Bibliothèque</h5></a>
-        </div>
-        <div className="Library">
-        <img className="library" src="person.svg" alt="" />
-        <a href="#" className="lienRef"><h5>Aritste</h5></a>
-        </div>
-        <div className="Library">
-          <img className="library" src="playlist_play.svg" alt="" />
-          <a href="#" className="lienRef"><h5>Playlist</h5></a>
-        </div>
-        </div>
-        
-      </div>
+    
      
     </div>
 
